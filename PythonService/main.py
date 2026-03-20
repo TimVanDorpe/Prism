a77ac49c16b375a5d715ba707ee89d657857e400
Tim Van Dorpe <Tim.van.dorpe@hotmail.com>
2026-03-20 20:21:32 +0100

    PythonService skeleton

---
+++ PythonService/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from core.limiter import limiter
+from api.routes import router
+
+app = FastAPI(title="Prism Comparison Service", version="1.0.0")
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.include_router(router)
